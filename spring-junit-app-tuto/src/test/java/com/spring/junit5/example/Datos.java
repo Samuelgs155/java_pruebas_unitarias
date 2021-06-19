678262393f6157a6e0a4063eeb93f6f1c20a8211
Samuelgs155 <samuelgs15@hotmail.com>
2021-06-19 15:32:06 +0200

    Add files via upload

---
+++ spring-junit-app-tuto/src/test/java/com/spring/junit5/example/Datos.java
@@ -0,0 +1,25 @@
+package com.spring.junit5.example;
+
+import com.spring.junit5.example.model.Banco;
+import com.spring.junit5.example.model.Cuenta;
+
+import java.math.BigDecimal;
+
+public class Datos {
+    public static final Cuenta CUENTA_001 = new Cuenta(1L, "Sam", new BigDecimal("1000"));
+    public static final Cuenta CUENTA_002 = new Cuenta(1L, "John", new BigDecimal("2000"));
+
+    public static final Banco BANCO = new Banco(1L, "EL banco financiero", 0);
+
+    public static Cuenta crearCuenta001() {
+        return new Cuenta(1L, "Sam", new BigDecimal("1000"));
+    }
+
+    public static Cuenta crearCuenta002() {
+        return new Cuenta(2L, "John", new BigDecimal("2000"));
+    }
+
+    public static Banco crearBanco() {
+        return new Banco(1L, "EL banco financiero", 0);
+    }
+}
